bbfc73f50e38b7fd0e81f5ff7906e07cddb38f77
matheusma37 <mtsma37@gmail.com>
2021-05-22 23:43:52 -0300

    Implementa o movimento horizontal do inimigo 1

--- js/main.js
+++ js/main.js
@@ -11,15 +11,19 @@ function start() {
     timer: setInterval(loop, 30)
   }
 
-  var TECLAS = {
+  const KEYS = {
     W: 87,
     S: 83,
     D: 68
   }
 
+  const SPEED = 5;
+  var enemy1PositionY = parseInt(Math.random() * 334);
+
   function loop() {
     moveBackground();
     movePlayer();
+    moveEnemy1();
   }
 
   $(document).keydown(function (e) {
@@ -31,21 +35,33 @@ function start() {
   });
 
   function movePlayer() {
-    if (game.pressed[TECLAS.W]) {
+    if (game.pressed[KEYS.W]) {
       var top = parseInt($("#player").css("top"));
       if (top >= 10) {
         $("#player").css("top", top - 10);
       }
     }
 
-    if (game.pressed[TECLAS.S]) {
+    if (game.pressed[KEYS.S]) {
       var top = parseInt($("#player").css("top"));
       if (top <= 444) {
         $("#player").css("top", top + 10);
       }
     }
 
-    if (game.pressed[TECLAS.D]) {
+    if (game.pressed[KEYS.D]) {
+    }
+  }
+
+  function moveEnemy1() {
+    positionX = parseInt($("#enemy1").css("left"));
+    $("#enemy1").css("left", positionX - SPEED);
+    $("#enemy1").css("top", enemy1PositionY);
+
+    if (positionX <= -30) {
+      enemy1PositionY = parseInt(Math.random() * 334);
+      $("#enemy1").css("left", 694);
+      $("#enemy1").css("top", enemy1PositionY);
     }
   }
 
