f34ed8d164b8ca6f44709194e71c28b8ef90011b
matheusma37 <mtsma37@gmail.com>
2021-05-22 23:20:31 -0300

    Implementa o movimento vertical do jogador

--- js/main.js
+++ js/main.js
@@ -6,11 +6,44 @@ function start() {
   $("#background-game").append("<div id='enemy2'></div>");
   $("#background-game").append("<div id='friend' class='animation_friend'></div>");
 
-  var jogo = {}
+  var game = {
+    pressed: {},
+    timer: setInterval(loop, 30)
+  }
 
-  jogo.timer = setInterval(loop, 30);
+  var TECLAS = {
+    W: 87,
+    S: 83,
+    D: 68
+  }
 
-  function loop() { moveBackground(); }
+  function loop() {
+    moveBackground();
+    movePlayer();
+  }
+
+  $(document).keydown(function (e) {
+    game.pressed[e.which] = true;
+  });
+
+  $(document).keyup(function (e) {
+    game.pressed[e.which] = false;
+  });
+
+  function movePlayer() {
+    if (game.pressed[TECLAS.W]) {
+      var top = parseInt($("#player").css("top"));
+      $("#player").css("top", top - 10);
+    }
+
+    if (game.pressed[TECLAS.S]) {
+      var top = parseInt($("#player").css("top"));
+      $("#player").css("top", top + 10);
+    }
+
+    if (game.pressed[TECLAS.D]) {
+    }
+  }
 
   function moveBackground() {
     left = parseInt($("#background-game").css("background-position"));
