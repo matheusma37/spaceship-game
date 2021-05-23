21cad4c8d7c2c1d19373e652405b454f497a99ea
matheusma37 <mtsma37@gmail.com>
2021-05-22 22:44:19 -0300

    Adiciona animação inicial

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function start() {
   $("#start").hide();
 
-  $("#background-game").append("<div id='player'></div>");
-  $("#background-game").append("<div id='enemy1'></div>");
+  $("#background-game").append("<div id='player' class='animation_player'></div>");
+  $("#background-game").append("<div id='enemy1' class='animation_enemy1'></div>");
   $("#background-game").append("<div id='enemy2'></div>");
-  $("#background-game").append("<div id='friend'></div>");
+  $("#background-game").append("<div id='friend' class='animation_friend'></div>");
 }
