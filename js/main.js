1c2fc5983e04049539049937ed7443317fb7aab4
matheusma37 <mtsma37@gmail.com>
2021-05-22 22:24:30 -0300

    Coloca personagens na tela

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function start() {
+  $("#start").hide();
+
+  $("#background-game").append("<div id='player'></div>");
+  $("#background-game").append("<div id='enemy1'></div>");
+  $("#background-game").append("<div id='enemy2'></div>");
+  $("#background-game").append("<div id='friend'></div>");
+}
