8e71ef094f369e297f6005371396309b0aa43b5e
matheusma37 <mtsma37@gmail.com>
2021-05-23 19:13:22 -0300

    Implementa outras explosões e colisões

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ function start() {
   const SPEED = 5;
   var enemy1PositionY = parseInt(Math.random() * 334);
   var canShoot = true;
+  var gameOver = false;
 
   function loop() {
     moveBackground();
@@ -116,15 +117,56 @@ function start() {
 
   function collision() {
     var collision1 = ($("#player").collision($("#enemy1")));
+    var collision2 = ($("#player").collision($("#enemy2")));
+    var collision3 = ($("#shoot").collision($("#enemy1")));
+    var collision4 = ($("#shoot").collision($("#enemy2")));
+    var collision5 = ($("#player").collision($("#friend")));
+    var collision6 = ($("#enemy2").collision($("#friend")));
 
     if (collision1.length > 0) {
-      enemy1X = parseInt($("#enemy1").css("left"));
-      enemy1Y = parseInt($("#enemy1").css("top"));
+      let enemy1X = parseInt($("#enemy1").css("left"));
+      let enemy1Y = parseInt($("#enemy1").css("top"));
       explosion1(enemy1X, enemy1Y);
 
       positionY = parseInt(Math.random() * 334);
       $("#enemy1").css("left", 694);
       $("#enemy1").css("top", positionY);
+    }
+
+    if (collision2.length > 0) {
+      let enemy2X = parseInt($("#enemy2").css("left"));
+      let enemy2Y = parseInt($("#enemy2").css("top"));
+      explosion2(enemy2X, enemy2Y);
+      $("#enemy2").remove();
+      replaceEnemy2();
+    }
+
+    if (collision3.length > 0) {
+      let enemy1X = parseInt($("#enemy1").css("left"));
+      let enemy1Y = parseInt($("#enemy1").css("top"));
+
+      explosion1(enemy1X, enemy1Y);
+      $("#shoot").css("left", 950);
+
+      positionY = parseInt(Math.random() * 334);
+      $("#enemy1").css("left", 694);
+      $("#enemy1").css("top", positionY);
+
+    }
+
+    if (collision4.length > 0) {
+      let enemy2X = parseInt($("#enemy2").css("left"));
+      let enemy2Y = parseInt($("#enemy2").css("top"));
+      $("#enemy2").remove();
+
+      explosion2(enemy2X, enemy2Y);
+      $("#shoot").css("left", 950);
+      replaceEnemy2();
+    }
+
+    if (collision5.length > 0) {
+      replaceFriend();
+      $("#friend").remove();
     }
   }
 
@@ -142,6 +184,47 @@ function start() {
       div.remove();
       window.clearInterval(timeExplosion);
       timeExplosion = null;
+    }
+  }
+
+  function explosion2(enemy2X, enemy2Y) {
+    $("#background-game").append("<div id='explosion2'></div");
+    $("#explosion2").css("background-image", "url(images/explosion.png)");
+    var div2 = $("#explosion2");
+    div2.css("top", enemy2Y);
+    div2.css("left", enemy2X);
+    div2.animate({ width: 200, opacity: 0 }, "slow");
+
+    var timeExplosion2 = window.setInterval(removeExplosion2, 1000);
+
+    function removeExplosion2() {
+      div2.remove();
+      window.clearInterval(timeExplosion2);
+      timeExplosion2 = null;
+    }
+  }
+
+  function replaceEnemy2() {
+    var timeCollision4 = window.setInterval(replace4, 5000);
+
+    function replace4() {
+      window.clearInterval(timeCollision4);
+      timeCollision4 = null;
+      if (!gameOver) {
+        $("#background-game").append("<div id=enemy2></div");
+      }
+    }
+  }
+
+  function replaceFriend() {
+    var timeFriend = window.setInterval(replace6, 6000);
+
+    function replace6() {
+      window.clearInterval(timeFriend);
+      timeFriend = null;
+      if (!gameOver) {
+        $("#background-game").append("<div id='friend' class='animation_friend'></div>");
+      }
     }
   }
 
